549fece420da2343f191ee68be84e49d4b48585f
ira_trubova <iraraddionova@gmail.com>
2026-04-02 09:48:58 +0200

    deleted comments

--- Tests/FlightEnrichmentAppTests/FlightEnrichmentAppTests.swift
+++ Tests/FlightEnrichmentAppTests/FlightEnrichmentAppTests.swift
@@ -1,17 +1,1 @@
-import XCTest
-@testable import schoolLF8
 
-final class WeatherMockServiceTests: XCTestCase {
-    
-    func testParisWeather() async throws {
-        let service = WeatherMockService()
-        let weather = try await service.fetchWeather(for: "Paris", date: Date())
-        XCTAssertEqual(weather.temperature, 18.0)
-    }
-    
-    func testLondonWeather() async throws {
-        let service = WeatherMockService()
-        let weather = try await service.fetchWeather(for: "London", date: Date())
-        XCTAssertEqual(weather.condition, "Rainy")
-    }
-}
